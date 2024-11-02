551b14cbfd6ed47dfe74aa2061d97496cbbc3954
Sagnik Pal <sagnikpal2004@outlook.com>
2024-11-02 03:08:11 -0400

    add backend code

---
+++ shazamNano/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+std::string SHAZAM_API = "https://amp.shazam.com/discovery/v5/en-US/GB/web/-/tag/A6AB600D-A35A-450D-AE95-EAFCA92A9854/F6040FEE-710D-4A5B-9082-F3E3A0E79A44?sync=true&webv3=true&sampling=true&connected=&shazamapiversion=v3&sharehub=true&hubv5minorversion=v5.1&hidelb=true&video=v3";
+
+// POST $(SHAZAM_API)
+// headers: {
+//     "X-Shazam-Platform": "IPHONE",
+//     "X-Shazam-AppVersion": "14.1.0",
+//     "Accept": "*/*",
+//     "Accept-Language": "en-US",
+//     "Accept-Encoding": "gzip, deflate",
+//     "User-Agent": "Dalvik/2.1.0 (Linux; U; Android 5.0.2; VS980 4G Build/LRX22G)",
+// }
+// body: {
+//     "timezone": "Europe/Moscow",
+//     "signature": {"uri": uri, "samplems": samplems},
+//     "timestamp": timestamp,
+//     "context": {},
+//     "geolocation": {},
+// }
+
+void setup() {
+
+}
+
+void loop() {
+
+}
